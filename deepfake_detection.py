bb4287472aeaf8f0730384561f8463afef6557dc
White_devil <ben10alienforce2553@gmail.com>
2024-03-07 14:37:16 +0530

    2nd

--- deepfake_detection.py
+++ deepfake_detection.py
@@ -17,7 +17,7 @@ import torch.nn.functional as F
 from facenet_pytorch import MTCNN, InceptionResnetV1
 import numpy as np
 from PIL import Image
-# import cv2
+import cv2
 from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 from pytorch_grad_cam.utils.image import show_cam_on_image
